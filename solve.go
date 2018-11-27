387eb40592e823384da9b5920d59f85eec025e57
Evert Provoost <evert.provoost@gmail.com>
2018-11-27 09:03:27 +0100

    Tailcall optimisation

--- solve.go
+++ solve.go
@@ -21,43 +21,47 @@ func Solve(problem *Problem) *Problem {
 
 // Solve is actually the real solver
 func solve(problem *Problem) *Problem {
-	// No need to create copy of problem, the caller should have done that.
+	// The solving is in a loop as we have a tail call.
+	for {
+		// No need to create copy of problem, the caller should have done that.
 
-	// Literal elimination
-	for variable := range problem.Variables {
-		if _, exists := problem.Variables[-variable]; !exists {
-			// The other polarity does not exist, we can eliminate the literal
-			problem.Assign(variable)
+		// Literal elimination
+		for variable := range problem.Variables {
+			if _, exists := problem.Variables[-variable]; !exists {
+				// The other polarity does not exist, we can eliminate the literal
+				problem.Assign(variable)
+			}
 		}
-	}
 
-	// Unit propagation
-	for !problem.Unsatisfiable && len(problem.Units) > 0 {
-		for unit := range problem.Units {
-			problem.Assign(unit)
+		// Unit propagation
+		for !problem.Unsatisfiable && len(problem.Units) > 0 {
+			for unit := range problem.Units {
+				problem.Assign(unit)
+			}
 		}
+
+		// We are done if there are no more variables to assign or
+		// the current assignment is unsatisfiable.
+		if len(problem.Variables) == 0 || problem.Unsatisfiable {
+			return problem
+		}
+
+		vr := ChooseVariable(problem)
+
+		// The solver changes the problem directly and we need the
+		// current version if this try is unsatisfiable.
+		firstTry := problem.Copy()
+
+		firstTry.Assign(vr)
+		res := solve(firstTry)
+
+		if !res.Unsatisfiable {
+			return res
+		}
+
+		// No need to create a copy, we'll never use problem again.
+		problem.Assign(-vr)
+
+		// We jump back up effectivively 'tail calling' Solve.
 	}
-
-	// We are done if there are no more variables to assign or
-	// the current assignment is unsatisfiable.
-	if len(problem.Variables) == 0 || problem.Unsatisfiable {
-		return problem
-	}
-
-	vr := ChooseVariable(problem)
-
-	// The solver changes the problem directly and we need the
-	// current version if this try is unsatisfiable.
-	firstTry := problem.Copy()
-
-	firstTry.Assign(vr)
-	res := solve(firstTry)
-
-	if !res.Unsatisfiable {
-		return res
-	}
-
-	// No need to create a copy, we'll never use problem again.
-	problem.Assign(-vr)
-	return solve(problem)
 }
